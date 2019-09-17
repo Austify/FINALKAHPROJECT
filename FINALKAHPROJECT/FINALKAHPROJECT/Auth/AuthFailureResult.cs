318aad6e64b839265d13924703b7351235d68ee3
Austin Ataga <austinataga@outlook.com>
2019-09-17 17:19:16 +0100

    Added JwtAuth Update

---
+++ FINALKAHPROJECT/FINALKAHPROJECT/Auth/AuthFailureResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FINALKAHPROJECT.Auth
+{
+    public class AuthFailureResult
+    {
+        private string v;
+        private HttpRequestMessage request;
+
+        public AuthFailureResult(string v, HttpRequestMessage request)
+        {
+            this.v = v;
+            this.request = request;
+        }
+    }
+}
