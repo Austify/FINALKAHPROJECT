5c3ebb85381470735b3244f545a03b25267033fb
Austin Ataga <austinataga@outlook.com>
2019-09-19 17:09:13 +0100

    Added JWT that works

--- FINALKAHPROJECT/FINALKAHPROJECT/Startup.cs
+++ FINALKAHPROJECT/FINALKAHPROJECT/Startup.cs
@@ -13,6 +13,10 @@ using Microsoft.Extensions.Options;
 using FINALKAHPROJECT.Models;
 using Microsoft.EntityFrameworkCore;
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 namespace FINALKAHPROJECT
 {
     public class Startup
@@ -27,9 +31,23 @@ namespace FINALKAHPROJECT
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            
             services.AddDbContext<ApplicationDbContext>
                            (options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = Configuration["Jwt:Issuer"],
+                ValidAudience = Configuration["Jwt:Issuer"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+            };
+        });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -44,8 +62,9 @@ namespace FINALKAHPROJECT
             {
                 app.UseHsts();
             }
+            app.UseAuthentication();
+          //  app.UseHttpsRedirection();
 
-            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
